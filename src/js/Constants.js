302c2d9db1910d5b9482f0d42167b72c156f51af
LukaszKoziarski <lkoziars@gmail.com>
2017-11-01 16:41:36 +0100

    Updated Constants file

--- src/js/Constants.js
+++ src/js/Constants.js
@@ -1,3 +1,10 @@
+import keyMirror from 'keymirror';
+
 export default {
-	baseUrl: 'http://46.101.28.25:8080'
+	HACKER_NEWS_API: 'http://46.101.28.25:8080',
+
+    // Each time you add an action, add it here... They should be past-tense
+    ActionTypes: keyMirror({
+        FETCHED_POSTS: null
+    })
 };
