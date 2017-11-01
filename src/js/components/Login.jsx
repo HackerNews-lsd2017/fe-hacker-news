8ed530666cd2654a046764d3784f77dac861bfff
LukaszKoziarski <lkoziars@gmail.com>
2017-11-01 16:40:52 +0100

    indentation and removed unused import

--- src/js/components/Login.jsx
+++ src/js/components/Login.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import actions from '../actions/actions';
 import '../../styles/App.css';
 
 export default class extends React.Component {
-
     render() {
         return (
             <div className="auth-container">
@@ -12,71 +10,58 @@ export default class extends React.Component {
                         <b>Login</b>
                     </div>
                     <br/>
+
                     <table>
                         <tbody>
                             <tr>
-                                <td>
-                                    username:
-                                </td>
-                                <td>
-                                    <input type="text"/>
-                                </td>
+                                <td>username:</td>
+                                <td><input type="text"/></td>
                             </tr>
                             <tr>
-                                <td>
-                                    password:
-                                </td>
-                                <td>
-                                    <input type="text"/>
-                                </td>
+                                <td>password:</td>
+                                <td><input type="text"/></td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
                 <br/>
+
                 <button>login</button>
                 <br/>
                 <br/>
+
                 <div>
-                    <a href="" className="pass-recovery">
-                        Forgot your password?
-                    </a>
+                    <a href="" className="pass-recovery">Forgot your password?</a>
                 </div>
                 <br/>
+
                 <div className="login-container">
                     <div>
                         <b>Create Account</b>
                     </div>
                     <br/>
+
                     <div>
                         <table>
                             <tbody>
                                 <tr>
-                                    <td>
-                                        username:
-                                    </td>
-                                    <td>
-                                        <input type="text"/>
-                                    </td>
+                                    <td>username:</td>
+                                    <td><input type="text"/></td>
                                 </tr>
                                 <tr>
-                                    <td>
-                                        password:
-                                    </td>
-                                    <td>
-                                        <input type="text"/>
-                                    </td>
+                                    <td>password:</td>
+                                    <td><input type="text"/></td>
                                 </tr>
                             </tbody>
                         </table>
                     </div>
                     <br/>
+                    
                     <div>
                         <button>create account</button>
                     </div>
                 </div>
             </div>
         )
-   
     }
 }
