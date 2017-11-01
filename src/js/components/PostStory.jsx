5faa85844945d077883398019bd8de5d4b92f206
LukaszKoziarski <lkoziars@gmail.com>
2017-11-01 16:39:37 +0100

    Renamed Story

--- src/js/components/PostStory.jsx
+++ src/js/components/PostStory.jsx
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import parseDomain from 'parse-domain';
 
 export default class extends React.Component {
