59997da5713a23a99af95eb654c78522cf51d607
WillVill <bech.william2@gmail.com>
2017-11-20 16:53:00 +0100

    changed react routing to fit nested routes

--- src/js/Routing.jsx
+++ src/js/Routing.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
+import {Route, Switch} from 'react-router';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import AppContainer from './components/AppContainer';
+import Content from './components/Content';
 import Login from './components/Login';
 import Submit from './components/Submit';
+import UserManagement from './components/UserManagement';
 
 /* Application Routes for Web */
 const AppRoutes = (
     <BrowserRouter>
         <App>
-            <Route exact path="/" component={AppContainer}/>
+            <Switch>
             <Route path="/login" component={Login}/>
             <Route path="/submit" component={Submit}/>
+                <AppContainer>
+                    <Route exact path="/" component={Content}/>
+                    <Route path="/user" component={UserManagement}/>
+                </AppContainer>
+            </Switch>
         </App>
     </BrowserRouter>
 );
-
 
 function init(targetId) {
     let target = document.getElementById(targetId);
--- src/js/components/AppContainer.jsx
+++ src/js/components/AppContainer.jsx
@@ -2,21 +2,17 @@ import React from 'react';
 import Actions from '../actions/PostActionCreators';
 import Store from '../stores/PostStore';
 import Header from './Header';
-import Content from './Content';
 import '../../styles/App.css';
 
 export default class extends React.Component {
     state = {
-        posts: [],
         user: {},
-        authenticated: Store.getAuth(),
-        loadMore: 1
+        authenticated: Store.getAuth()
     }
 
     componentDidMount = () => {
         Store.addChangeListener(this.onChange);
         Actions.checkAuth();
-        this.loadData();
     }
 
     componentWillUnmount = () => {
@@ -25,27 +21,17 @@ export default class extends React.Component {
 
     onChange = () => {
         this.setState({
-            posts: Store.getPosts(),
             user: Store.getUser(),
             authenticated: Store.getAuth()
         });
     }
 
-    // @todo: it's a hack - waiting for the backend
-    loadData = () => {
-        let {loadMore} = this.state;
-        Actions.getPosts(30 * loadMore);
-        this.setState({
-            loadMore: loadMore + 1
-        });
-    }
-
     render = () => {
-        let {posts, user, authenticated} = this.state;
+        let {user, authenticated} = this.state;
         return (
             <div className="app">
                 <Header user={user} authenticated={authenticated}/>
-                <Content posts={posts} loadPosts={this.loadData}/>
+                {this.props.children}
             </div>
         )
     }
